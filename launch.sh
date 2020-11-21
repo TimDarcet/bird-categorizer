6e99893ca00fc06cf398ead7fc39058a4f6eadbd
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-21 12:19:41 +0100

    verbose launch script

--- launch.sh
+++ launch.sh
@@ -4,5 +4,6 @@ rank=0
 for i in $(cat computers_name | head -n $n_nodes)
 do
     ssh -oStrictHostKeyChecking=no $i "tmux new-session -d -s birdrec \"cd ~/bird-recognition-mva/bird-categorizer && conda activate pytorch && python -m torch.distributed.launch --nproc_per_node=1 --nnodes=$n_nodes --node_rank=$rank --master_addr=\\\"129.104.254.39\\\" --master_port=1234 ./distrib_train.py\"" &
+    echo Launched node $i with rank $rank
     ((rank++))
 done
