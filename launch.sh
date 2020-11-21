ec20c77cdfb9cca82b933c8efa32fb42bf6c9296
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-21 12:18:06 +0100

    Launch script permission and corrections

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 n_nodes=$1
-echo $n_nodes
-
+echo "Launching $n_nodes nodes"
+rank=0
 for i in $(cat computers_name | head -n $n_nodes)
 do
-        # ssh -oStrictHostKeyChecking=no $i "tmux new-session -d -s birdrec \"cd ~/bird-recognition-mva/bird-categorizer && conda activate pytorch && python -m torch.distributed.launch --nproc_per_node=1 --nnodes=$n_nodes --node_rank=1 --master_addr=\\\"129.104.254.39\\\" --master_port=1234 ./distrib_train.py\"" &
-        echo $c
+    ssh -oStrictHostKeyChecking=no $i "tmux new-session -d -s birdrec \"cd ~/bird-recognition-mva/bird-categorizer && conda activate pytorch && python -m torch.distributed.launch --nproc_per_node=1 --nnodes=$n_nodes --node_rank=$rank --master_addr=\\\"129.104.254.39\\\" --master_port=1234 ./distrib_train.py\"" &
+    ((rank++))
 done
