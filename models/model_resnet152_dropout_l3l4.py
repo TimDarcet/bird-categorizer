1c8aa602f9cf5b4ed7695c66aba75f55ae5cd6d5
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-24 00:06:41 +0100

    train l3 too

---
+++ models/model_resnet152_dropout_l3l4.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+
+nclasses = 20 
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.featurizer = models.resnet152(pretrained=True)
+        num_ftrs = self.featurizer.fc.in_features
+        for param in self.featurizer.parameters():
+            param.requires_grad = False
+        for param in self.featurizer.layer4.parameters():
+            param.requires_grad = True
+        for param in self.featurizer.layer3.parameters():
+            param.requires_grad = True
+        self.featurizer.fc = Identity()
+        self.dp = nn.Dropout(0.2)
+        self.classifier = nn.Linear(num_ftrs, nclasses)
+
+    def forward(self, x):
+        feats = self.featurizer(x)
+        drops = self.dp(feats)
+        return self.classifier(drops)
