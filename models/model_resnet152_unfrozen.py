aae7bf78e8f86ffb2ddd7cf6fcf5f1f7969f052c
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-23 23:41:35 +0100

    resnet152 unfrozen

---
+++ models/model_resnet152_unfrozen.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+
+nclasses = 20 
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.featurizer = models.resnet152(pretrained=True)
+        num_ftrs = self.featurizer.fc.in_features
+        self.featurizer.fc = Identity()
+        # self.dp = nn.Dropout(0.2)
+        self.classifier = nn.Linear(num_ftrs, nclasses)
+
+    def forward(self, x):
+        feats = self.featurizer(x)
+        drops = self.dp(feats)
+        return self.classifier(drops)
