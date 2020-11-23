69c472fae30ea5671caee647939d2b882c6ddc10
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-23 18:42:31 +0100

    Higher lr

--- distrib_train.py
+++ distrib_train.py
@@ -17,7 +17,7 @@ def init_parser():
     dataset_default = 'crops_square/bird_dataset'
     epochs_default = 200
     batch_size_default = 2048
-    lr_default = 0.3
+    lr_default = 2
     momentum_default = 0.5
     log_interval_default = 10
     experiment_default = 'experiment'
