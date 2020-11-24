a1d844b8a69963d163d13f717cb25fb657bbb516
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-24 23:02:32 +0100

    Set master

--- launch.sh
+++ launch.sh
@@ -5,9 +5,10 @@ rank=0
 rm -rf logs
 mkdir -p logs/stdout
 mkdir -p logs/stderr
+master=$(cat computers_name | tail -n +$offset | head -n 1)
 for i in $(cat computers_name | tail -n +$offset | head -n $n_nodes)
 do
-    ssh -oStrictHostKeyChecking=no $i "tmux new-session -d -s birdrec \"source ~/.bashrc && cd ~/bird-recognition-mva/bird-categorizer && conda activate pytorch && python -m torch.distributed.launch --nproc_per_node=1 --nnodes=$n_nodes --node_rank=$rank --master_addr=\\\"anchois.polytechnique.fr\\\" --master_port=1234 ./distrib_train.py >logs/stdout/log.$i 2>logs/stderr/log.$i\"" &
+    ssh -oStrictHostKeyChecking=no $i "tmux new-session -d -s birdrec \"source ~/.bashrc && cd ~/bird-recognition-mva/bird-categorizer && conda activate pytorch && python -m torch.distributed.launch --nproc_per_node=1 --nnodes=$n_nodes --node_rank=$rank --master_addr=\\\"$master\\\" --master_port=1234 ./distrib_train.py >logs/stdout/log.$i 2>logs/stderr/log.$i\"" &
     echo Launched node $i with rank $rank
     ((rank++))
 done
