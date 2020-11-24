981bfd5ae85f86b62672f20530ccf4caef08e09b
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-24 18:01:22 +0100

    MOAR data augment

--- data.py
+++ data.py
@@ -17,14 +17,14 @@ eval_transforms = transforms.Compose([
 ])
 
 train_transforms = transforms.Compose([
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.03),
+    transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
     # transforms.ToTensor(),
     # transforms.ToPILImage(),
     transforms.Resize((224, 224), interpolation=BICUBIC),
-    transforms.RandomAffine(degrees=15, translate=[0.1, 0.1], scale=[0.9, 1.1], shear=[-5, 5], resample=BICUBIC),
+    transforms.RandomAffine(degrees=30, translate=[0.2, 0.2], scale=[0.7, 1.4], shear=[-10, 10], resample=BICUBIC),
     transforms.ToTensor(),
-    transforms.RandomErasing(p=0.35),
-    transforms.RandomApply([transforms.GaussianBlur(5, (0.1, 2))], p=0.2),
+    transforms.RandomErasing(p=0.5),
+    transforms.RandomApply([transforms.GaussianBlur(5, (0.1, 2))], p=0.4),
     transforms.RandomHorizontalFlip(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
