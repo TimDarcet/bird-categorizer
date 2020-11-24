2105d5b1fade7933649f81645b758ec7de76a535
Timothée Darcet <timothee.darcet+git@m4x.org>
2020-11-24 23:03:28 +0100

    Kill also

--- kill.sh
+++ kill.sh
@@ -1,7 +1,8 @@
 n_nodes=$1
+offset=$2
 echo "Killing $n_nodes nodes"
 rank=0
-for i in $(cat computers_name | head -n $n_nodes)
+for i in $(cat computers_name | tail -n +$offset | head -n $n_nodes)
 do
     ssh -oStrictHostKeyChecking=no $i "tmux kill-session -t birdrec" &
     echo Killed node $i with rank $rank
